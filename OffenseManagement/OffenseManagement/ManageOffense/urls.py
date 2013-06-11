2ca68c9441e1bf1a8644b690e9e85599f3292f26
mayrens <steven.mayresn@gmail.com>
2013-06-11 22:15:46 +0100

    test2

--- OffenseManagement/OffenseManagement/ManageOffense/urls.py
+++ OffenseManagement/OffenseManagement/ManageOffense/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
 from OffenseManagement.ManageOffense import views
 
-
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^site/$', views.site, name='site'),
